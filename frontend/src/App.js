58537567e3db48bab54cd63e9d1632886c33dfef
test_seccgeo <hamdi.secilmis@gmail.com>
2025-08-25 20:19:22 +0300

    feat: create frontend by "npx create-react-app"

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Hello World</h1>
+      <p> SeçGEO App</p>
+      <p> Welcome to the SeçGEO application!</p>
+
+    </div>
+  );
+}
+
+export default App;
